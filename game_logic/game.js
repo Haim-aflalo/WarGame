e6d2d80fc779018742adcabbb7a156ec857bf3ed
Haim Aflalo <kodkodhaim@gmail.com>
2025-12-07 12:28:19 +0200

    finised game_utils

--- game_logic/game.js
+++ game_logic/game.js
@@ -2,18 +2,30 @@ function createPlayer(name = 'AI') {
   return { name: name, hand: [], won_pile: [] };
 }
 
-// function initGame(){
-//     let game_dict = {}
-//     p1 = createPlayer("hermon")
-//     game_dict["player_1"] = p1
-//     p2 = createPlayer()
-//     game_dict["player_2"] = p2
-//     game_deck = create_deck()
-//     deck_shuffled = shuffle(game_deck)
-//     game_dict["deck"] = deck_shuffled
-//     // game_dict["player_1"]["hand"] = deck_shuffled[26:]
-//     // game_dict["player_2"]["hand"] = deck_shuffled[:26]
+function initGame() {
+  let game_dict = {};
+  let p1 = createPlayer('haim');
+  game_dict['player_1'] = p1;
+  let p2 = createPlayer();
+  game_dict['player_2'] = p2;
+  let game_deck = create_deck();
+  let deck_shuffled = shuffle(game_deck);
+  game_dict['deck'] = deck_shuffled;
+  game_dict['player_1']['hand'] = deck_shuffled.slice(
+    0,
+    deck_shuffled.length / 2
+  );
+  game_dict['player_2']['hand'] = deck_shuffled.slice(deck_shuffled.length / 2);
+  return game_dict;
+}
 
-//     return game_dict
-
-// }
+function playRound(p1, p2) {
+  const p1Card = p1.hand.pop();
+  const p2Card = p2.hand.pop();
+  let result = compare_cards(p1['hand'].slice(-1), p2['hand'].slice(-1));
+  if (result == 'p1') {
+    p1Hand.push(p1Card, p2Card);
+  } else if (result == 'p2') {
+    p2Hand.push(p2Card, p1Card);
+  } else return;
+}
